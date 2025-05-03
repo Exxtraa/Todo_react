ee55c13eb507376166cfd744fa45fde631f3b74e
Arpit <arpitxtraa69@gmail.com>
2025-05-03 12:08:25 +0530

    repair profile case sansative

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Home from "./pages/Home"
 import Header from "./components/Header";
 import Register from "./pages/Register";
-import Profile from "./pages/profile";
+import Profile from "./pages/Profile";
 import Login from "./pages/login";
 import { Toaster } from "react-hot-toast";
 import { useContext, useEffect } from "react";
